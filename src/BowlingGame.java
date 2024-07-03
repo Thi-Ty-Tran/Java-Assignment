7a6b4101250991fb499d8d50ad73ddd8c7203d4a
Thi-Ty-Tran <169084154+Thi-Ty-Tran@users.noreply.github.com>
2024-07-03 11:31:37 -0400

    Author comments

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,2 +1,9 @@
+/*
+ * Name: Thi Ty Tran
+ * Date: July 1st, 2024
+ * File: BowlingGame
+ * Description: This program implements a scoring system for the game of bowling.
+ */
+
 public class BowlingGame {
 }
